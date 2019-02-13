f55fbe8dc85c08918eff86692d61f4ad01315202
Cavdy <cavdyofficials@gmail.com>
2019-02-13 23:15:44 +0100

    edited app.js with routes

--- api/app.js
+++ api/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import mealsRoute from './routes/meals.route';
 
 const app = express();
-const PORT = 5500;
+const PORT = 5600;
 
-app.get('/', (req, res) => res.send('The API is working'));
+app.use('/api/v1/meals', mealsRoute);
 
 app.listen(PORT, () => {
   console.log(`server running on port: ${PORT}`);

3a02a087daf57c9cdaa25c2841dca73b1d3fc530
Cavdy <cavdyofficials@gmail.com>
2019-02-14 03:14:53 +0100

    edited meal routes

--- api/app.js
+++ api/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import mealsRoute from './routes/meals.route';
 
 const app = express();
-const PORT = 5600;
+const PORT = 5700;
+
+app.use(bodyParser.json());
 
 app.use('/api/v1/meals', mealsRoute);
 

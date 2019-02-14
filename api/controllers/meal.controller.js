370d9fd19a2127ef6d53fba992708114d41fb9ad
Cavdy <cavdyofficials@gmail.com>
2019-02-14 16:52:26 +0100

    created order api and endpoints

--- api/controllers/meal.controller.js
+++ api/controllers/meal.controller.js
@@ -2,6 +2,7 @@ import MealService from '../services/meal.service';
 
 // created our meal controller
 const MealController = {
+  // get/fetch all meals
   fetchAllMeals(req, res) {
     const allMeals = MealService.fetchAllMeals();
     return res.json({
@@ -10,6 +11,7 @@ const MealController = {
     }).status(200);
   },
 
+  // Add meal
   addMeal(req, res) {
     const newMeal = req.body;
     const createdMeal = MealService.addMeal(newMeal);
@@ -19,8 +21,9 @@ const MealController = {
     }).status(201);
   },
 
+  // Get meal by id
   getSingleMeal(req, res) {
-    const id = req.params.id;
+    const [id] = [req.params.id];
     const foundMeal = MealService.getMeal(id);
     return res.json({
       status: 'success',
@@ -28,8 +31,9 @@ const MealController = {
     }).status(200);
   },
 
+  // Update Meal
   updateMeal(req, res) {
-    const id = req.params.id;
+    const [id] = [req.params.id];
     const newMeal = req.body;
     const updatedMeal = MealService.updateMeal(id, newMeal);
     return res.json({
@@ -38,8 +42,9 @@ const MealController = {
     }).status(201);
   },
 
+  // Delete Meal
   deleteMeal(req, res) {
-    const id = req.params.id;
+    const [id] = [req.params.id];
     const deleteMeal = MealService.deleteMeal(id);
     return res.json({
       status: 'success',
--- api/controllers/menu.controller.js
+++ api/controllers/menu.controller.js
@@ -2,6 +2,7 @@ import MenuService from '../services/menu.service';
 
 // created our menu controller
 const MenuController = {
+  // get menu of the day
   getMenuOfDay(req, res) {
     const menu = MenuService.getMenuOfDay();
     return res.json({
@@ -10,6 +11,7 @@ const MenuController = {
     }).status(200);
   },
 
+  // Add menu of the day
   addMenuOfDay(req, res) {
     const newMenu = req.body;
     const updatedMenu = MenuService.addMenuOfDay(newMenu);

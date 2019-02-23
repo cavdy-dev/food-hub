8cf0990d005de76e7178acef013a276da8116d76
Franklin Ikenna Isaiah <37126612+Cavdy@users.noreply.github.com>
2019-02-23 23:31:49 +0100

    Revert "Order api test"

--- api/app.js
+++ api/app.js
@@ -1,24 +1,10 @@
 import express from 'express';
-// body parser to read the data
-import bodyParser from 'body-parser';
-import mealsRoute from './routes/meals.route';
-import menuRoute from './routes/menu.routes';
-import ordersRoute from './routes/orders.route';
 
-// instantiate expressjs
 const app = express();
-const PORT = 5900;
+const PORT = 5500;
 
-app.use(bodyParser.json());
+app.get('/', (req, res) => res.send('The API is working'));
 
-// creating the api version route
-app.use('/api/v1/meals', mealsRoute);
-app.use('/api/v1/menus', menuRoute);
-app.use('/api/v1/orders', ordersRoute);
-
-// listening to our port
 app.listen(PORT, () => {
   console.log(`server running on port: ${PORT}`);
 });
-
-export default app;

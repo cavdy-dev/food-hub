f48e05f36c6e3272f1ca3a36691fd29b6b549238
Cavdy <cavdyofficials@gmail.com>
2019-02-25 17:43:09 +0100

    order data created

--- api/services/orders.service.js
+++ api/services/orders.service.js
@@ -1,11 +1,11 @@
 import MealModel from '../models/Meal.model';
-import dummyData from '../utils/dummyData';
+import orderData from '../utils/orderData';
 
 const OrdersService = {
 
   // fetching all the orders
   getAllOrders() {
-    return dummyData.meals.map((order) => {
+    return orderData.meals.map((order) => {
       const orders = new MealModel();
       orders.id = order.id;
       orders.name = order.name;
@@ -17,23 +17,23 @@ const OrdersService = {
 
   // getting order by id
   getSingleOrder(id) {
-    const orders = dummyData.meals.find(order => order.id == id);
+    const orders = orderData.meals.find(order => order.id == id);
     return orders || {};
   },
 
   // adding to order list
   addToOrder(order) {
-    const orderLength = dummyData.meals.length;
+    const orderLength = orderData.meals.length;
 
     // checking if the array is empty to avoid undefined errors.
     if (orderLength === undefined || orderLength == 0) {
       order.id = 1;
-      dummyData.meals.push(order);
+      orderData.meals.push(order);
     } else {
-      const lastId = dummyData.meals[orderLength - 1].id;
+      const lastId = orderData.meals[orderLength - 1].id;
       const newId = lastId + 1;
       order.id = newId;
-      dummyData.meals.push(order);
+      orderData.meals.push(order);
     }
     return order;
   },
@@ -41,17 +41,17 @@ const OrdersService = {
   // updating order
   updateOrder(id, updateOrder) {
     // checking for meal id, deleting and updating the array
-    const orderId = dummyData.meals.find(order => order.id == id);
+    const orderId = orderData.meals.find(order => order.id == id);
     updateOrder.id = orderId.id;
-    dummyData.meals.splice(orderId.id - 1, 1, updateOrder);
+    orderData.meals.splice(orderId.id - 1, 1, updateOrder);
     return updateOrder;
   },
 
   // delete order by id
   deleteOrder(id) {
     // checking for order id and deleting it from the array
-    const order = dummyData.meals.find(order => order.id == id);
-    dummyData.meals.splice(order.id - 1, 1);
+    const order = orderData.meals.find(order => order.id == id);
+    orderData.meals.splice(order.id - 1, 1);
     return {};
   },
 
